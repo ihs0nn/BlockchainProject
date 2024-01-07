f587dd52aa308c47b2cab0abf61bf04d826fe7a7
ihs0nn <ihs0nn@users.noreply.github.com>
2024-01-07 23:27:18 +0100

    Add wanted files

--- smartcontract.sol
+++ smartcontract.sol
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
@@ -55,61 +54,3 @@ contract VerifierIntegriteDonnees {
     }
 }
 
-=======
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.19;
-
-contract VerifierIntegriteDonnees {
-
-    // Adresse de l'administrateur du contrat
-    address admin;
-
-    // Constructeur du contrat qui initialise l'adresse de l'administrateur
-    constructor() {
-        admin = msg.sender;
-    }
-
-    // Tableau pour stocker et extraire les empreintes de hachage des fichiers associées à leurs URL
-    mapping (string => bytes32) internal FileHash;
-
-    // Fonction de hachage interne
-    function hash(string memory content) internal pure returns(bytes32) {
-        // Utilisation de la fonction de hachage keccak256 pour obtenir l'empreinte de hachage du contenu
-        bytes32 contentHash = keccak256(bytes(content));
-        return contentHash;
-    }
-
-    // Fonction pour mettre à jour l'empreinte de hachage d'un fichier
-    function mettreAJourEmpreinteFichier(string memory _url, string memory _contenu) public {
-        // Seul l'administrateur peut créer ou mettre à jour une empreinte de hachage de fichier
-        require(msg.sender == admin, "Vous n'avez pas l'autorisation de mettre à jour les empreintes de hachage des fichiers");
-        FileHash[_url] = hash(_contenu);
-    }
-
-    // Fonction pour vérifier l'intégrité du contenu d'un fichier
-    function verifierIntegriteDonnees(string memory _url, string memory _nouveauContenu) public view returns(string memory) {
-        string memory message;
-
-        // Vérifier si l'URL est valide
-        if (FileHash[_url] == bytes32(0)) {
-            message = "Cette URL est invalide, vérifiez le fichier téléchargé et réessayez";
-            return message;
-        }
-
-        // Récupérer l'empreinte de hachage actuelle et celle du nouveau contenu
-        bytes32 empreinteCourante = FileHash[_url];
-        bytes32 nouvelleEmpreinte = hash(_nouveauContenu);
-
-        // Vérifier si le contenu a été modifié
-        if (empreinteCourante == nouvelleEmpreinte) {
-            message = "Le contenu n'a pas été modifié !";
-            return message;
-        } else {
-            message = "Le contenu a été modifié !";
-            return message;
-        }
-    }
-}
-
->>>>>>> 5c72e611c09d54ccde7fff3fa5416e3b141cd090
